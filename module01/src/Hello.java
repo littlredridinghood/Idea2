a3ee787ec55fd51442e82f60beaac93437abce04
littlredridinghood <1475658164@qq.com>
2020-06-14 10:22:48 +0800

    first commit

---
+++ module01/src/Hello.java
@@ -0,0 +1,9 @@
+/**
+ * @author HuangYuhan
+ * @date 2020/6/14 - 10:17
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
